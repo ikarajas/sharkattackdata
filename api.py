908e3403dc2f48e25ce6625cde0e9ef8981a50f9
ikarajas <ivankarajas@gmail.com>
2014-04-15 21:54:32 +1000

    Fixed dates in API.

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ from models import SharkAttack, Country, Country, Area
 from utils import StringUtils
 from repositories import CountryRepository, AreaRepository, SharkAttackRepository
 
+import google.appengine.ext.ndb.model
 
 class Attacks(webapp2.RequestHandler):
     def __init__(self, request, response):
@@ -43,9 +44,15 @@ class Attacks(webapp2.RequestHandler):
 
         retval = []
         for attack in self._sharkAttackRepository.getDescendantAttacksForKey(node.key):
+            try:
+                dateTemp = None if attack.date_is_approximate else attack.date
+            except google.appengine.ext.ndb.model.UnprojectedPropertyError as e:
+                logging.warning("Shouldn't happen.")
+                dateTemp = None
+
             retval.append({
                 "gsafCaseNumber": attack.gsaf_case_number,
-                #"date": attack.date, #Why are we getting an error stating that this isn't in the projection?
+                "date": None if dateTemp is None else dateTemp.isoformat(),
                 "dateOrig": attack.date_orig,
                 "dateIsApproximate": attack.date_is_approximate,
                 "dateUserFriendly": attack.date_userfriendly,
