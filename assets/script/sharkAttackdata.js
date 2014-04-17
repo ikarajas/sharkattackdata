917ccbaadec9bd825959e1b2c77c78a5ac23a6d4
ikarajas <ivankarajas@gmail.com>
2014-04-18 01:09:49 +1000

    Fatal/provoked filtering on attack lists.

--- assets/script/sharkAttackdata.js
+++ assets/script/sharkAttackdata.js
@@ -9,8 +9,63 @@
 
 	    function AttackPlaceSummaryViewModel() {
 		var self = this;
+
+		self.filterStatuses_fatal = {
+		    fatal_only: "Fatal only",
+		    non_fatal_only: "Non-fatal only",
+		    fatal_and_non_fatal: "Fatal and non-fatal"
+		}
+
+		self.filterStatuses_provoked = {
+		    provoked_only: "Provoked only",
+		    unprovoked_only: "Unprovoked only",
+		    provoked_and_unprovoked: "Provoked and unprovoked"
+		}
+
 		self.attacks = ko.observableArray();
 		self.attacksLoaded = ko.observable(false);
+		
+		self.attacksFiltered = ko.computed(function() {
+		    return $.grep(self.attacks(), function(a, i) {
+			fatalCheckPass = true;
+
+			var fatalState = self.filterDropdownFatalState();
+			if (fatalState === self.filterStatuses_fatal.fatal_only && !a.fatal ||
+			    fatalState === self.filterStatuses_fatal.non_fatal_only && a.fatal) {
+			    fatalCheckPass = false;
+			}
+
+			provokedCheckPass = true;
+
+			var provokedState = self.filterDropdownProvokedState();
+			if (provokedState === self.filterStatuses_provoked.provoked_only && !a.provoked ||
+			    provokedState === self.filterStatuses_provoked.unprovoked_only && a.provoked) {
+			    provokedCheckPass = false;
+			}
+
+			return fatalCheckPass && provokedCheckPass;
+		    })
+		});
+
+		self.noAttacksMatchFilterSettings = ko.computed(function() {
+		    return self.attacksLoaded() && self.attacksFiltered().length === 0;
+		});
+
+		self.filterDropdownFatalState = ko.observable(self.filterStatuses_fatal.fatal_and_non_fatal);
+		self.filterDropdownProvokedState = ko.observable(self.filterStatuses_provoked.provoked_and_unprovoked);
+		
+		self.isFiltered = ko.computed(function() {
+		    return self.filterDropdownFatalState() !== self.filterStatuses_fatal.fatal_and_non_fatal ||
+			self.filterDropdownProvokedState() !== self.filterStatuses_provoked.provoked_and_unprovoked;
+		});
+
+		self.filterStateSummary = ko.computed(function() {
+		    if (self.isFiltered()) {
+			return "Filtered (" + self.attacksFiltered().length + " shown, " + (self.attacks().length - self.attacksFiltered().length) + " hidden).";
+		    }
+		    return "";
+		});
+
 		self.summaryStats = {
 		    totalCount: ko.computed(function() {
 			return self.attacksLoaded() ? self.attacks().length : widget.loadingMessage;
@@ -31,7 +86,24 @@
 
 	    ko.applyBindings(widget.vm, widget.element[0]);
 
+	    widget._setupEventHandlers();
 	    widget._getAttacks();
+	},
+
+	_setupEventHandlers: function() {
+	    var widget = this;
+
+	    $(".btn-group.filter-on-fatal .dropdown-menu a").on("click.placeWidget", function(event) {
+		event.preventDefault();
+		var newStateKey = event.target.id.replace(/-/g, "_");
+		widget.vm.filterDropdownFatalState(widget.vm.filterStatuses_fatal[newStateKey]);
+	    });
+
+	    $(".btn-group.filter-on-provoked .dropdown-menu a").on("click.placeWidget", function(event) {
+		event.preventDefault();
+		var newStateKey = event.target.id.replace(/-/g, "_");
+		widget.vm.filterDropdownProvokedState(widget.vm.filterStatuses_provoked[newStateKey]);
+	    });
 	},
 
 	_onAttacksLoaded: function() {
@@ -114,6 +186,7 @@
 		success: function(result) {
 		    widget.vm.attacks.push.apply(widget.vm.attacks, $.map(result, function(value, index) {
 			return $.extend(value, {
+			    provoked: !value.unprovoked,
 			    unprovokedUserFriendly: value.unprovoked ? "Unprovoked" : "Provoked",
 			    fatalUserFriendly: value.fatal ? "Fatal" : "Non-fatal",
 			    detailsUrl: (function(v) {
